606f55ede63a70d0c6f9e3675e201eba938c1c3e
Danjes623 <danielmejiaf0107@gmail.com>
2025-10-06 21:44:22 -0500

    errors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
   .connect("mongodb://localhost:27017/InventarioP")
   .then(() => console.log("✅ MongoDB conectado"))
   .catch((err) => console.error(err));
-s
+
 // Modelo de Usuario (puedes crear esto en models/Usuario.js después)
 const usuarioSchema = new mongoose.Schema({
   name: { 
